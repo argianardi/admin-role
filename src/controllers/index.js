afc1f9dbc2c90ac78a5e7d1e2478262b744ddd58
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-01-12 00:17:13 +0700

    add porter controllers

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,6 +1,8 @@
 const controllers = {};
 const category = require("./category");
+const porter = require("./porter");
 
 controllers.category = category;
+controllers.porter = porter;
 
 module.exports = controllers;
