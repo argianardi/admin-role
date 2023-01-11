060f592aa6a26d236b3d3d120528e789a70d24d6
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-01-11 18:06:24 +0700

    add category controller and route for get requst al categories

--- src/routes/category.js
+++ src/routes/category.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const controllers = require("../controllers/index");
 
-//post request
-router.post("/", controllers.category.post);
+// post category request
+router.post("/category", controllers.category.post);
+
+// get all categories request
+router.get("/categories", controllers.category.getAll);
 
 module.exports = router;
