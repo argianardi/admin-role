4f92bb9c1c4dc6338403daf1e5c68af106afb112
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-01-12 12:30:27 +0700

    add get all mitra request

--- src/controllers/mitra.js
+++ src/controllers/mitra.js
@@ -59,4 +59,29 @@ controllerMitra.post = async (req, res) => {
   }
 };
 
+// get All mitra request
+controllerMitra.getAll = async (req, res) => {
+  try {
+    const mitras = await models.mitra.findAll();
+    if (mitras.length > 0) {
+      res.status(200).json({
+        success: true,
+        message: "All mitra successfully obtained",
+        data: mitras,
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "The Mitra not found",
+        data: [],
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "500 internal server error",
+    });
+  }
+};
+
 module.exports = controllerMitra;
--- src/routes/mitra.js
+++ src/routes/mitra.js
@@ -5,4 +5,7 @@ const controllers = require("../controllers/index");
 // post request
 router.post("/mitra", controllers.mitra.post);
 
+// get all porter request
+router.get("/mitras", controllers.mitra.getAll);
+
 module.exports = router;
