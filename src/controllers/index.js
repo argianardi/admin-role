32d7faf8d964071fc972015df13e98cf1afabcff
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-01-12 12:14:41 +0700

    add mitra controllers

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,8 +1,10 @@
 const controllers = {};
 const category = require("./category");
 const porter = require("./porter");
+const mitra = require("./mitra");
 
 controllers.category = category;
 controllers.porter = porter;
+controllers.mitra = mitra;
 
 module.exports = controllers;
