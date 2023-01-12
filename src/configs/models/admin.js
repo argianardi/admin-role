4d8f110ef7b8a0556a911de22aed701bccb4ed5f
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-01-12 16:48:45 +0700

    add admin table model

---
+++ src/configs/models/admin.js
@@ -0,0 +1,36 @@
+const Sequelize = require("sequelize"); // import sequelize
+const db = require("../database/database"); // import database
+
+const admin = db.define(
+  "admin",
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: { type: Sequelize.INTEGER, allowNull: false },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: "email",
+    },
+    role: { type: Sequelize.STRING, defaultValue: "admin" },
+    createdAt: Sequelize.DATE,
+    updatedAt: Sequelize.DATE,
+  },
+  { freezeTableName: true }
+);
+
+// to execute alter table
+db.sync({ alter: true })
+  .then(() => {
+    console.log("Admin table created successfully!");
+  })
+  .catch((error) => {
+    console.log("Unable to create Admin table:", error.message);
+  });
+
+// export table
+module.exports = admin;
--- src/configs/models/index.js
+++ src/configs/models/index.js
@@ -3,10 +3,12 @@ const category = require("./category");
 const porter = require("./porter");
 const mitra = require("./mitra");
 const product = require("./product");
+const admin = require("./admin");
 
 models.category = category;
 models.porter = porter;
 models.mitra = mitra;
 models.product = product;
+models.admin = admin;
 
 module.exports = models;
