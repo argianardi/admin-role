8f4b7e124abd5645a0c21dc3f752d99f4b80c68d
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-01-12 14:31:51 +0700

    add product table model

--- src/configs/models/index.js
+++ src/configs/models/index.js
@@ -2,9 +2,11 @@ const models = {};
 const category = require("./category");
 const porter = require("./porter");
 const mitra = require("./mitra");
+const product = require("./product");
 
 models.category = category;
 models.porter = porter;
 models.mitra = mitra;
+models.product = product;
 
 module.exports = models;
