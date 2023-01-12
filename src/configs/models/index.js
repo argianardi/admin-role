f30a041d52e9557884d1a24f29f60c68c12dca74
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-01-12 12:13:39 +0700

    add mitra model

--- src/configs/models/index.js
+++ src/configs/models/index.js
@@ -1,8 +1,10 @@
 const models = {};
 const category = require("./category");
 const porter = require("./porter");
+const mitra = require("./mitra");
 
 models.category = category;
 models.porter = porter;
+models.mitra = mitra;
 
 module.exports = models;
