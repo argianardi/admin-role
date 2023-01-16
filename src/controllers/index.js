00a6bf871602f58fd5ad91f758b6f2048ecf70c2
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-01-16 15:51:40 +0700

    add admin auth router

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -4,11 +4,13 @@ const porter = require("./porter");
 const mitra = require("./mitra");
 const product = require("./product");
 const admin = require("./admin");
+const authAdmin = require("./authAdmin");
 
 controllers.category = category;
 controllers.porter = porter;
 controllers.mitra = mitra;
 controllers.product = product;
 controllers.admin = admin;
+controllers.authAdmin = authAdmin;
 
 module.exports = controllers;
