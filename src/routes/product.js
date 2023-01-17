7d39c72c2e7860731aafea3340456ceaca2cf71e
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-01-17 10:34:14 +0700

    add validate authenticate for product routers

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const controllers = require("../controllers/index");
+const validateAuth = require("../middlewares/validateAuth");
 
 // get all products request
-router.get("/products", controllers.product.getAll);
+router.get(
+  "/products",
+  validateAuth.isAuthenticated,
+  controllers.product.getAll
+);
 
 // get one product by id request
-router.get("/product/:id", controllers.product.getOneProduct);
+router.get(
+  "/product/:id",
+  validateAuth.isAuthenticated,
+  controllers.product.getOneProduct
+);
 
 // delete one product by id request
-router.delete("/product/:id", controllers.product.delete);
+router.delete(
+  "/product/:id",
+  validateAuth.isAuthenticated,
+  controllers.product.delete
+);
 
 module.exports = router;
