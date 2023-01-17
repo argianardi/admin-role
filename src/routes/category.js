a0a1cba68fd598c52898e0c5ef716ccff262c58c
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-01-17 11:05:21 +0700

    add validate authenticate for category roters

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,17 +1,34 @@
 const express = require("express");
 const router = express.Router();
 const controllers = require("../controllers/index");
+const validateAuth = require("../middlewares/validateAuth");
 
 // post category request
-router.post("/category", controllers.category.post);
+router.post(
+  "/category",
+  validateAuth.isAuthenticated,
+  controllers.category.post
+);
 
 // get all categories request
-router.get("/categories", controllers.category.getAll);
+router.get(
+  "/categories",
+  validateAuth.isAuthenticated,
+  controllers.category.getAll
+);
 
 // put one category by id request
-router.put("/category/:id", controllers.category.put);
+router.put(
+  "/category/:id",
+  validateAuth.isAuthenticated,
+  controllers.category.put
+);
 
 // delete category request
-router.delete("/category/:id", controllers.category.delete);
+router.delete(
+  "/category/:id",
+  validateAuth.isAuthenticated,
+  controllers.category.delete
+);
 
 module.exports = router;
