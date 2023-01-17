cbbf928e8af28dc292ee8171093e66eadb7e0d98
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-01-17 10:49:45 +0700

    add validate authenticate

--- src/routes/mitra.js
+++ src/routes/mitra.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const controllers = require("../controllers/index");
+const validateAuth = require("../middlewares/validateAuth");
 
 // get all porter request
-router.get("/mitras", controllers.mitra.getAll);
+router.get("/mitras", validateAuth.isAuthenticated, controllers.mitra.getAll);
 
 // put status_kemitraan request
-router.put("/kemitraan/:id", controllers.mitra.put);
+router.put(
+  "/kemitraan/:id",
+  validateAuth.isAuthenticated,
+  controllers.mitra.put
+);
 
 module.exports = router;
