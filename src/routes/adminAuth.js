9121baa0f39d9cd6715a3cbd90711b04da9727ae
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-01-17 11:15:38 +0700

    add router for admin login and register features

--- src/routes/adminAuth.js
+++ src/routes/adminAuth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const controllers = require("../controllers/index");
 
-router.post("/admin/login", controllers.authAdmin.login);
-// router.post("/register", controllers.auth.register);
+router.post("/admin/login", controllers.adminAuth.login);
+router.post("/admin/register", controllers.adminAuth.register);
 
 module.exports = router;
