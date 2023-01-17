796ef707ed75b34fce29ea667cb75225af64838a
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-01-17 10:35:57 +0700

    add validate auth for porter routers

--- src/routes/porter.js
+++ src/routes/porter.js
@@ -1,20 +1,29 @@
 const express = require("express");
 const router = express.Router();
 const controllers = require("../controllers/index");
+const validateAuth = require("../middlewares/validateAuth");
 
 // post request
-router.post("/porter", controllers.porter.post);
+router.post("/porter", validateAuth.isAuthenticated, controllers.porter.post);
 
 // get all porter request
-router.get("/porters", controllers.porter.getAll);
+router.get("/porters", validateAuth.isAuthenticated, controllers.porter.getAll);
 
 // get one porter by id request
-router.get("/porter/:id", controllers.porter.getOnePorter);
+router.get(
+  "/porter/:id",
+  validateAuth.isAuthenticated,
+  controllers.porter.getOnePorter
+);
 
 // put porter request
-router.put("/porter/:id", controllers.porter.put);
+router.put("/porter/:id", validateAuth.isAuthenticated, controllers.porter.put);
 
 // delete porter request
-router.delete("/porter/:id", controllers.porter.delete);
+router.delete(
+  "/porter/:id",
+  validateAuth.isAuthenticated,
+  controllers.porter.delete
+);
 
 module.exports = router;
