17e6b002ceeeb3b8870089a21a25254b573dce26
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-01-18 23:07:10 +0700

    fixed body response for get shipping cost request

--- src/controllers/shippingcost.js
+++ src/controllers/shippingcost.js
@@ -23,16 +23,13 @@ controllerShippingCost.getCost = (req, res) => {
       weight,
       courier,
     })
-    // .then((response) => res.json(response.data))
     .then((response) => {
-      console.log(response.data);
-      console.log("---------------------------------------------------");
       let courier = response.data.rajaongkir.results[0].name;
       let cost = response.data.rajaongkir.results[0].costs;
       return res.status(200).json({
-        datas: {
-          success: true,
-          message: "Success get shipping cost",
+        success: true,
+        message: "Success get shipping cost",
+        data: {
           courier: courier,
           cost: cost,
         },
@@ -46,52 +43,6 @@ controllerShippingCost.getCost = (req, res) => {
         },
       });
     });
-  console.log(data);
 };
 
 module.exports = controllerShippingCost;
-
-// export const ordersDistance = async (req, res) => {
-//   const destination = req.body.destination;
-//   const key = process.env.API_KEY;
-//   const id = req.body.id;
-
-//   const address = await getOrdersDistance(id);
-
-//   const origin = address.district;
-
-//   var distance;
-
-//   var config = {
-//     method: "get",
-//     url: `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${origin}&destinations=${destination}&key=${key}`,
-//     headers: {},
-//   };
-
-//   await axios(config)
-//     .then(function (response) {
-//       console.log(JSON.stringify(response.data));
-//       distance = response.data;
-//     })
-//     .catch(function (error) {
-//       console.log(error);
-//     });
-
-//   let text = "";
-//   if (distance.rows[0].elements[0].distance.value > 15000) {
-//     text = "distance too far( > 15 km )";
-//   } else {
-//     text = "sucess get distance";
-//   }
-//   return res.status(200).json({
-//     meta: {
-//       code: 200,
-//       message: text,
-//     },
-//     data: {
-//       origin: origin,
-//       destination: destination,
-//       distance: distance.rows[0].elements[0].distance.text,
-//     },
-//   });
-// };
